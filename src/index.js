1b6434a33555a017ad15c3ea5fd2c3b7b43ce14c
Jennifer Walker <dataqueen16@gmail.com>
2018-07-08 19:35:37 -0500

    modified the app component to search the youtube api and return an original list using a key term

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyADSheNOszT_ofPlDbMYdvRk1h9tw5VCDU';
 
 
 
 // Create a new component. This component will produce some HTMl
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            //this.setState({ videos: videos});
+        });
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
         );
+    }
 }
 
 
